d98dac28ad96d4eab37d17caedfded3c3da93c36
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-05-14 20:47:52 +0700

    43 reusable widget & constructor functions

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 255, 73, 18),
+          Color.fromARGB(255, 81, 255, 0),
+        ),
       ),
     ),
   );
